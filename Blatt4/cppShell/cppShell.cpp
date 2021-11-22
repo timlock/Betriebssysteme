47b7d50f04ae0b6bef98ca361bb9529b91e61c64
timlock <tim-lock@hotmail.de>
2021-11-22 08:49:11 +0100

    1.1

---
+++ Blatt4/cppShell/cppShell.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string input;
+    while(input != "-1"){
+    cout  << argv[1] << "@Linux:" << argv[2];
+    cin >> input;
+    }
+}
