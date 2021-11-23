7c8009e3e9db459c786944af9cc2f2cffc025c49
timlock <tim-lock@hotmail.de>
2021-11-23 09:44:03 +0100

    1.1

--- Blatt4/cppShell/cppShell.cpp
+++ Blatt4/cppShell/cppShell.cpp
@@ -1,38 +1,83 @@
 #include "cppShell.h"
 
-
- vector<string> MiniShell::pathify(string path){
-        int pos = 0;
-        vector<string> vecPath;
-        while ((pos = path.find("/")) != std::string::npos) {
-            vecPath.push_back(path.substr(0, pos));
-            path.erase(0, pos + 1);
-        }       
-        vecPath.push_back(path);
-        for(string i: vecPath){
-            cout << i << endl;
-        }
-        return vecPath;
+vector<string> MiniShell::pathify(string path)
+{
+    int pos = 0;
+    vector<string> vecPath;
+    while ((pos = path.find("/")) != std::string::npos)
+    {
+        vecPath.push_back(path.substr(0, pos));
+        path.erase(0, pos + 1);
     }
-
-MiniShell::MiniShell(char* user, char* path):user(user), strPath(path){
-        vecPath = pathify(strPath);
+    vecPath.push_back(path);
+    for (string i : vecPath)
+    {
+        cout << i << endl;
     }
+    return vecPath;
+}
 
+MiniShell::MiniShell(char *user, char *path) : user(user), strPath(path)
+{
+    vecPath = pathify(strPath);
+}
 
-    void MiniShell::run (){
-        string input;
-        while(input != "-1"){
-            cout  << user << "@Linux:" << strPath;
-            cin >> input;
-        }
-
+void MiniShell::run()
+{
+    string input;
+    while (input != "-1")
+    {
+        cout << user << "@Linux:" << strPath;
+        cin >> input;
+        digest(input);
     }
+}
+
+void MiniShell::digest(string input)
+{
+    vector<string> inputVec;
+    string arg;
+    stringstream sstream(input);
+    int count = 0;
+    while (sstream >> arg)
+    {
+        inputVec.push_back(arg);
+    }
+    if (inputVec.size() == 0)
+        return;
+    execute(inputVec);
+}
+
+int MiniShell::execute(vector<string> &inputVec)
+{
+    if (inputVec[0] == "cd")
+        changeDir(inputVec);
+    if (inputVec[0] == "exit")
+        miniExit(inputVec);
+    if (inputVec[0] == "showenv")
+        showenv(inputVec);
+    if (inputVec[0] == "export")
+        miniExport(inputVec);
 
 
+        return 0;
+}
+
+void MiniShell::changeDir(vector<string> &inputVec){
+    
+}
+void MiniShell::miniExit(vector<string> &inputVec){
+
+}
+void MiniShell::showenv(vector<string> &inputVec){
+
+}
+void MiniShell::miniExport(vector<string> &inputVec){
+
+}
 
 int main(int argc, char *argv[])
 {
-    MiniShell shell(argv[1],argv[2]);
+    MiniShell shell(argv[1], argv[2]);
     shell.run();
 }
--- Blatt4/cppShell/cppShell.h
+++ Blatt4/cppShell/cppShell.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -11,9 +12,15 @@ class MiniShell{
     vector<string> vecPath;
     string strPath;
     string user;
+    int status = 0;
 
     MiniShell(char* user, char* path);
     vector<string> pathify(string path);
     void run();
-
+    void digest(string input);
+    int execute(vector<string> &inputVec);
+    void changeDir(vector<string> &inputVec);
+    void miniExit (vector<string> &inputVec);
+    void showenv (vector<string> &inputVec);
+    void miniExport(vector<string> &inputVec);
 };
