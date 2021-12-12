5f80327a9be312b215e84e23cba48c4903b3ba3b
timlock <tim-lock@hotmail.de>
2021-12-12 15:14:38 +0100

    Main arbeitet jetzt mit Übergabeparametern

--- Blatt6/webBot/main.cpp
+++ Blatt6/webBot/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "WebBot.h"
 
-//int main(int argc, char** argv) {
+int main(int argc, char** argv) {
+   /*
   int main(){
-    std::cout << "Hello, world!\n";
-    WebBot *b = new WebBot(10,10, "testSites_10_2020.txt", 100, true);
+  int queueSize = 5;
+  int threadCount = 20;
+  int delay = 100;
+  */
+    int queueSize = stoi(string(argv[1]));
+    int threadCount = stoi(string(argv[2]));
+    int delay = stoi(string(argv[3]));
+  WebBot *b = new WebBot(queueSize,threadCount, "testSites_10_2020.txt", delay, true);
+//    WebBot *b = new WebBot(10,20, "testSites_10_2020.txt", 100, true);
     b->initialize();
     b->run();
 }
